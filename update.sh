231c0c7e4cd54d35a5a9ae17da33306556b27b11
taikucingmokondo <jav.guru1001@gmail.com>
2024-08-15 00:35:08 +0700

    Update update.sh
    nambah install git

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e '\033[36mGanti Skrip,\033[33m Bos\033[0m'
-
+sudo apt install git -y
 sudo mv -f /boot/firmware/.PPPwn  /boot/firmware/.PPPwn-bak
 sudo git clone https://github.com/taikucingmokondo/miayampedas 
 sudo mv -f miayampedas /boot/.pppwn
